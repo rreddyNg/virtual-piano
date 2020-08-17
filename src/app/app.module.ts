efa30a1f4298326bd292994af7bcc8c298bc0fa0
Ravindra Reddy <ravindrap@hotmail.com>
2020-08-16 17:21:24 -1000

    Project Piano: Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PianoPlayService } from './services/piano.play.service';
+import { PianoKeyComponent } from './components/piano-key/piano-key.component';
+import { PianoComponent } from './components/piano/piano.component';
+import { PianoOctaveComponent } from './components/piano-octave/piano-octave.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PianoKeyComponent,
+    PianoComponent,
+    PianoOctaveComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [PianoPlayService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/piano-key/piano-key.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { PianoPlayService } from '../../services/piano.play.service';
+
+// A single piano key which is able to be played via the mouse or using the keys
+@Component({
+  selector: 'app-piano-key',
+  templateUrl: './piano-key.component.html',
+  styleUrls: ['./piano-key.component.scss'] 
+})
+export class PianoKeyComponent implements OnInit {
+  get sharpKey() { return !this.note || this.note.endsWith("#"); }
+  get keyName(): string { return `${this.note}${this.index}` };
+  get pianoSoundUrl(): string { return `assets/piano_sounds/${this.note.replace("#", "-")}${this.index}.mp3` };
+
+  @Input() note: string;
+  @Input() index: number;
+
+  isActive: boolean = false;
+  noteSound = null;
+
+  constructor(private pianoService: PianoPlayService) { }
+
+  ngOnInit() { 
+    this.pianoService.registerPianoKey(this);
+  }
+ 
+  ngAfterViewChecked() {
+    this.noteSound = new Audio();
+    this.noteSound.src = this.pianoSoundUrl;
+    this.noteSound.load();
+  }
+
+  onPianoKeyDown(event: Event) {
+    if (event) event.preventDefault();
+
+    if (this.noteSound) this.noteSound.play();
+    this.isActive = true;
+  }
+
+  onPianoKeyUp(event: Event) {
+    if (event) event.preventDefault();
+    this.isActive = false;
+  }
+}
